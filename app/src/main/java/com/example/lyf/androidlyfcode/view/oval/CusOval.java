7e2e91083df89a5ea7b85eb576bce9cffe73dac2
Administrator <530810890@qq.com>
2018-01-08 20:01:45 +0800

    提交图形文件夹

---
+++ app/src/main/java/com/example/lyf/androidlyfcode/view/oval/CusOval.java
@@ -0,0 +1,11 @@
+package com.example.lyf.androidlyfcode.view.oval;
+
+/**
+ * Created by huangweizhu on 2018/1/8.
+ * e-mail:huangweizhu8@163.com
+ * version: 2.0
+ * Describe:
+ */
+
+public class CusOval {
+}
